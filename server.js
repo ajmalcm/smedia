c71b9dffcd211fc351285eb304a8a45f6e0457ac
ajmalcm <95700196+ajmalcm@users.noreply.github.com>
2022-10-19 20:00:28 +0530

    make it better

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ mongoose.connect(URI, {
 
 if(process.env.NODE_ENV === "production")
 {
-    app.use(express.static("smedia/client/build"))
+    app.use(express.static("client/build"))
     app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname, 'smedia/client/build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
