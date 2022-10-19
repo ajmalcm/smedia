c95f71cb32439680bc628867b84e34088c1094c1
ajmalcm <95700196+ajmalcm@users.noreply.github.com>
2022-10-19 17:14:43 +0530

    make it better

--- server.js
+++ server.js
@@ -44,13 +44,22 @@ mongoose.connect(URI, {
 }) 
 
 
-if(process.env.NODE_ENV === 'production')
-{
-    app.use(express.static('client/build'))
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    })
-}
+// if(process.env.NODE_ENV === 'production')
+// {
+//     app.use(express.static('client/build'))
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+//     })
+// }
+
+if (process.env.NODE_ENV === "production") {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, "client/build")));
+    // Handle React routing, return all requests to React app
+    app.get("*", function (req, res) {
+      res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
+  }
 
 //listeners
 const port = process.env.PORT || 5000;
